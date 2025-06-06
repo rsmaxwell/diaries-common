fbca53bc69a5b01603e05f5cd052314d4424b3d1
Richard Maxwell <richard@rsmaxwell.co.uk>
2025-06-06 11:08:49 +0100

    Fix refreshToken problems

--- src/main/java/com/rsmaxwell/diaries/common/response/SigninReply.java
+++ src/main/java/com/rsmaxwell/diaries/common/response/SigninReply.java
@@ -3,14 +3,14 @@ package com.rsmaxwell.diaries.common.response;
 import lombok.Data;
 
 @Data
-public class SigninResponsePayload {
+public class SigninReply {
 
 	private String accessToken;
 	private String refreshToken;
 	private Integer refreshPeriod;
 	private Long id;
 
-	public SigninResponsePayload(String accessToken, String refreshToken, Integer refreshPeriod, Long id) {
+	public SigninReply(String accessToken, String refreshToken, Integer refreshPeriod, Long id) {
 		this.accessToken = accessToken;
 		this.refreshToken = refreshToken;
 		this.refreshPeriod = refreshPeriod;
