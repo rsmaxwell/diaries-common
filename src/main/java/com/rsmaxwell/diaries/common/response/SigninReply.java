6474f4cd0b8c247ccdbc2969164c668b82501fea
Richard Maxwell <richard@rsmaxwell.co.uk>
2026-03-01 10:45:54 +0000

    Add the edit Lock to the Fragment

--- src/main/java/com/rsmaxwell/diaries/common/response/SigninReply.java
+++ src/main/java/com/rsmaxwell/diaries/common/response/SigninReply.java
@@ -8,13 +8,17 @@ public class SigninReply {
 	private String accessToken;
 	private String refreshToken;
 	private Integer refreshPeriod;
-	private Long id;
+	private Long userId;
+	private String username;
+	private String knownas;
 
-	public SigninReply(String accessToken, String refreshToken, Integer refreshPeriod, Long id) {
+	public SigninReply(String accessToken, String refreshToken, Integer refreshPeriod, Long id, String username, String knownas) {
 		this.accessToken = accessToken;
 		this.refreshToken = refreshToken;
 		this.refreshPeriod = refreshPeriod;
-		this.id = id;
+		this.userId = id;
+		this.username = username;
+		this.knownas = knownas;
 	}
 
 }
