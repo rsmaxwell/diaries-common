50626966fdf98dcd5dcf6ad29ea815d4f4ba81f2
Richard Maxwell <richard@rsmaxwell.co.uk>
2026-03-03 09:34:59 +0000

    Initial version of the Lock Fragment button

--- src/main/java/com/rsmaxwell/diaries/common/response/SigninReply.java
+++ src/main/java/com/rsmaxwell/diaries/common/response/SigninReply.java
@@ -11,14 +11,16 @@ public class SigninReply {
 	private Long userId;
 	private String username;
 	private String knownAs;
+	private String sessionId;
 
-	public SigninReply(String accessToken, String refreshToken, Integer refreshPeriod, Long id, String username, String knownAs) {
+	public SigninReply(String accessToken, String refreshToken, Integer refreshPeriod, Long id, String username, String knownAs, String sessionId) {
 		this.accessToken = accessToken;
 		this.refreshToken = refreshToken;
 		this.refreshPeriod = refreshPeriod;
 		this.userId = id;
 		this.username = username;
 		this.knownAs = knownAs;
+		this.sessionId = sessionId;
 	}
 
 }
